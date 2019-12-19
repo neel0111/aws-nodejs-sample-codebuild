821a1c68b516d921f58d6ce3bbbd356fe5f16562
hanee01 <54358825+hanee01@users.noreply.github.com>
2019-12-19 11:57:33 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://3.230.66.65/',
+       serverUrl:'http://3.230.66.65',
        options : {
        'sonar.sources': '.',
        }
