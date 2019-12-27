df98777953a75974d382872c9ecd6670b9136fe4
hanee01 <54358825+hanee01@users.noreply.github.com>
2019-12-27 16:24:29 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-echo "Deployment Started"
-cd /artifacts/stg
-sam deploy --template-file outputtemplate.yaml --stack-name lossam --region us-east-1 --capabilities CAPABILITY_IAM --parameter-overrides StageName=Staging
-echo "Deployment Completed"
